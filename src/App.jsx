56e22a113ae0f090e42510227d0005f51c3a8133
Aryan Patel  <aryanpatel@192.168.1.11>
2026-03-28 11:39:12 +0530

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <main className="min-h-dvh p-6">
+      <h1 className="text-xl font-medium text-black">
+        App
+      </h1>
+    </main>
+  )
+}
